361ce00ccc7f884477ccda50265eabb5423b7344
Mainul Islam <mainulislam6168@gmail.com>
2023-12-26 10:56:55 +0600

    chefData generated

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+const chefData =require('./data/chefData.json');
+
 
 app.get('/', (req, res)=>{
     res.send('chef data is coming')
 });
+
+app.get('/chefData', (req, res) =>{
+    res.send(chefData)
+})
 
 app.listen(port, ()=>{
     console.log(`assignment 10 server is running on ${port}`);
